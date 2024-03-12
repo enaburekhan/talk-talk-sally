717420d008aaaccdb49d2fdc1c0db7748dbb9cc7
enaburekhan <ericenaburekhan2018@gmail.com>
2024-03-12 10:17:06 +0000

    Add responsiveness to PostDetail page

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -2,7 +2,17 @@ import { useParams } from 'react-router-dom';
 import { useFetchPostQuery } from '../utils/postsApi';
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useEffect } from 'react';
-
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  Divider,
+  Image,
+  Text,
+  SimpleGrid,
+} from '@chakra-ui/react';
 const PostDetail = () => {
   const { id } = useParams();
   const { data: post, error, isError } = useFetchPostQuery(id ? id : skipToken);
@@ -11,16 +21,29 @@ const PostDetail = () => {
     isError && toast.error(error);
   }, [isError]);
   return (
-    <div key={post?.id}>
-      <img src={post?.imgURL} alt={post?.title} />
-      <h3>{post?.title}</h3>
-      <p>{post?.author}</p>
-      <p>{post?.content}</p>
-      <div>
-        <span>Created at - &nbsp;</span>
-        <small>{post?.timestamp.toDate().toLocaleString()}</small>
-      </div>
-    </div>
+    <Card maxW='sm' key={post?.id}>
+      <CardBody>
+        <Image src={post?.imgURL} alt={post?.title} borderRadius='lg' />
+        <Stack mt='6' spacing='3'>
+          <CardHeader size='md'>{post?.title}</CardHeader>
+          <Text>{post?.content}</Text>
+          <Text fontSize='14px' color='#333'>
+            Author: {post?.author}
+          </Text>
+          <Text color='blue.600' fontSize='10px'>
+            Created on: {post?.timestamp.toDate().toLocaleString()}
+          </Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <SimpleGrid
+          columns={{ base: 1, md: 2, lg: 4 }}
+          spacing={4}
+          align='center'
+        ></SimpleGrid>
+      </CardFooter>
+    </Card>
   );
 };
 
