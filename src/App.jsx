d755b8d96299e687b9ac44dbe171d79cbb264980
enaburekhan <ericenaburekhan2018@gmail.com>
2024-03-19 23:47:25 +0000

    Add rich-text package to format postDetail page

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,27 @@ import Navbar from './component/Navbar';
 import PostsList from './pages/PostsList';
 import PostDetail from './pages/PostDetail';
 import './index.css';
-function App() {
-  return (
-    <div>
-      <Router>
-        <div>
-          <Navbar />
-          <ToastContainer position='top-center' />
-          <Routes>
-            <Route path='/' element={<PostsList />} />
-            <Route path='/addPost' element={<AddEditPosts />} />
-            <Route path='/update/:id' element={<AddEditPosts />} />
+import { Box } from '@chakra-ui/react';
 
-            <Route path='/detail/:id' element={<PostDetail />} />
-          </Routes>
-        </div>
+export default function App() {
+  return (
+    <Box>
+    <Router>
+      <div>
+        <Navbar />
+        <ToastContainer position='top-center' />
+        <Routes>
+          <Route path='/' element={<PostsList />} />
+          <Route path='/addPost' element={<AddEditPosts />} />
+          <Route path='/update/:id' element={<AddEditPosts />} />
+
+          <Route path='/detail/:id' element={<PostDetail />} />
+        </Routes>
+      </div>
       </Router>
-    </div>
+      
+    
+    </Box>
+  
   );
 }
-
-export default App;
--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -3,49 +3,49 @@ import { useFetchPostQuery } from '../utils/postsApi';
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useEffect } from 'react';
 import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Stack,
-  Divider,
   Image,
   Text,
-  SimpleGrid,
   Box,
+  Heading,
 } from '@chakra-ui/react';
-const PostDetail = () => {
+import { toast } from 'react-toastify'; 
+import { Editor } from '@tinymce/tinymce-react';
+
+const PostDetail = () => {  
   const { id } = useParams();
   const { data: post, error, isError } = useFetchPostQuery(id ? id : skipToken);
 
   useEffect(() => {
     isError && toast.error(error);
   }, [isError]);
+
+
+ 
+  
+
   return (
     <Box maxW='600px' mx='auto' mt='6'>
-      <Card maxW='sm' key={post?.id}>
-        <CardBody>
-          <Image src={post?.imgURL} alt={post?.title} borderRadius='lg' />
-          <Stack mt='6' spacing='3'>
-            <CardHeader size='md'>{post?.title}</CardHeader>
-            <Text>{post?.content}</Text>
-            <Text fontSize='14px' color='#333'>
-              Author: {post?.author}
-            </Text>
-            <Text color='blue.600' fontSize='10px'>
-              Created on: {post?.timestamp.toDate().toLocaleString()}
-            </Text>
-          </Stack>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <SimpleGrid
-            columns={{ base: 1, md: 2, lg: 4 }}
-            spacing={4}
-            align='center'
-          ></SimpleGrid>
-        </CardFooter>
-      </Card>
+      
+      
+        <Heading>{post?.title}</Heading>
+        <Box>{<Image src={post?.imgURL} alt={post?.title} borderRadius='lg' />}</Box>
+        <Editor
+      apiKey='qzdns6jmf0qi7om95ppnxngk06o19tuuzdx3zdde6ub7v7cb'
+      initialValue={post?.content}
+      init={{
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate mentions tableofcontents footnotes mergetags autocorrect typography inlinecss',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+      }}
+      
+    />  
+        <Text fontSize='14px' color='#333'>
+          Written by {post?.author}
+        </Text>
+        <Text color='blue.600' fontSize='10px'>
+          Created on: {post?.timestamp.toDate().toLocaleString()}
+        </Text>
+        
+      
     </Box>
   );
 };
