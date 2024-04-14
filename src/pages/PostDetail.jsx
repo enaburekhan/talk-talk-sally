4162eef975003fd3ba4a947a2a211218651b6b34
enaburekhan <ericenaburekhan2018@gmail.com>
2024-04-14 18:52:39 +0100

    Update post details page to be responsive on mobile

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -2,7 +2,16 @@ import { useParams } from 'react-router-dom';
 import { useFetchCommentsQuery, useFetchPostQuery } from '../utils/postsApi';
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useEffect } from 'react';
-import { Flex, Text, Box, Heading, Avatar, Button } from '@chakra-ui/react';
+import {
+  Flex,
+  Text,
+  Box,
+  Heading,
+  Avatar,
+  Button,
+  Spinner,
+  useColorMode,
+} from '@chakra-ui/react';
 import { toast } from 'react-toastify';
 import CommentsForm from '../component/CommentsForm';
 
@@ -15,6 +24,8 @@ const PostDetail = () => {
     isError: commentsError,
   } = useFetchCommentsQuery(id);
 
+  const { colorMode } = useColorMode(); // Get the current color mode
+
   useEffect(() => {
     isError && toast.error(error);
   }, [isError]);
@@ -24,44 +35,57 @@ const PostDetail = () => {
   }, [commentsError]);
 
   return (
-    <Flex align='center' justify='center'>
-      <Box maxW='600px' mx='auto' mt='6'>
-        <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap' mb='8px'>
-          {
-            <Avatar
-              src={post?.imgURL}
-              name={post?.title}
-              borderRadius='lg'
-              rounded='full'
-            />
-          }
-          <Box>
-            <Text fontSize='14px' color='#fff' fontWeight='800'>
-              Written by {post?.author}
-            </Text>
-          </Box>
-        </Flex>
-        <Heading mb='5px'>{post?.title}</Heading>
+    <Flex
+      direction='column'
+      align='center'
+      justify='flex-start'
+      p={4}
+      bg={colorMode === 'dark' ? 'gray.800' : 'gray.50'}
+      color={colorMode === 'dark' ? 'white' : 'black'}
+    >
+      <Box maxW={['90%', '90%', '600px']} w='100%'>
+        <Box mb='4'>
+          <Avatar
+            src={post?.imgURL}
+            name={post?.title}
+            borderRadius='full'
+            mr='2'
+          />
 
-        <div dangerouslySetInnerHTML={{ __html: post?.content }} />
+          <Text
+            fontSize='sm'
+            fontWeight='bold'
+            // color={colorMode === 'dark' ? 'black' : 'white'}
+          >
+            Written by {post?.author}
+          </Text>
+        </Box>
 
-        <Text color='blue.600' fontSize='12px' fontWeight='800'>
+        <Heading mb='4'>{post?.title}</Heading>
+
+        <Box dangerouslySetInnerHTML={{ __html: post?.content }} mb='4' />
+
+        <Text fontSize='sm' fontWeight='bold' mb='4'>
           Created on: {post?.timestamp.toDate().toLocaleString()}
         </Text>
         <CommentsForm post={post} />
-        {isLoading && <Box>Loading Comments...</Box>}
+        {isLoading && <Spinner />}
         {comments && comments.length > 0 && (
           <Box mt={6}>
-            <Heading size='md'>Comments</Heading>
+            <Heading size='md' mb='2'>
+              Comments
+            </Heading>
             {comments.map((comment) => (
               <Box
                 key={comment.id}
                 bg='gray.100'
                 p={4}
                 borderRadius='md'
-                mt={2}
+                mb='2'
               >
-                <Text color='#333'>{comment.email}</Text>
+                <Text fontWeight='bold' mb='2' color='#333'>
+                  {comment.email}
+                </Text>
                 <Text color='#333'>${comment.comment}</Text>
               </Box>
             ))}
